2aa004334d3b3f4fbc7cf132890aa5103de689c6
userandaname <zayaan.rajput@gmail.com>
2024-09-19 16:40:39 +0100

    add ping cmd

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -2,10 +2,15 @@ import {
     Client,
     MessageEmbed
 } from 'discord.js-selfbot-v13';
+const prefix = "!";
 
 export async function messageCreate(client, message) {
     if (message.author.bot) return;
     if (message.author.id === client.user.id) return;
 
-    message.reply(`hello world`);
+    if (message.content === `${prefix}ping`) {
+        return message.reply('pong');
+    }
+
+    return message.reply(`hello world`);
 }
